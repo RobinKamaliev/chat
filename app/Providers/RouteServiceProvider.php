1bd13576bbc236825d40989994785fcf146f3c57
Камалиев Робин <robinkamaliev@mail.ru>
2025-04-19 21:59:30 +0300

    Добавил RateLimit.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,11 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 
 class RouteServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->configRateLimiter();
+
         foreach (glob(base_path('routes/api/v*/*')) as $file) {
             $version = basename(dirname($file));
 
@@ -19,5 +24,12 @@ class RouteServiceProvider extends ServiceProvider
 
         Route::middleware('web')
             ->group(base_path('routes/web.php'));
+    }
+
+    private function configRateLimiter(): void
+    {
+        RateLimiter::for('global', static function (Request $request): Limit {
+            return Limit::perSecond(10)->by($request->ip());
+        });
     }
 }
